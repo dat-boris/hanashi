35505a82f5fb6d0e546042fa82ac41c076290d35
sketchytechky <boris@techie.im>
2018-06-17 10:32:08 -0700

    Added postgres backend

--- web/hanashi/routing.py
+++ web/hanashi/routing.py
@@ -1,5 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import slideshow.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            slideshow.routing.websocket_urlpatterns
+        )
+    ),
 })
