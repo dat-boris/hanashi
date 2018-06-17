43fc072db096f29d0842ef89b40c4a728f49a5b7
sketchytechky <boris@techie.im>
2018-06-17 11:57:34 -0700

    Fix up static file access

--- web/hanashi/settings.py
+++ web/hanashi/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
+STATIC_URL = 'https://storage.googleapis.com/hanashi-207514/static/'
+
+STATIC_ROOT = 'static/'
 
 ASGI_APPLICATION = 'hanashi.routing.application'
 CHANNEL_LAYERS = {
