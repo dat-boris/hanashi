e1568f99c957a43f03e85ecc1d82753ba2e2c5c1
sketchytechky <boris@techie.im>
2018-06-17 10:15:10 -0700

    Added slideshow tests

--- web/hanashi/settings.py
+++ web/hanashi/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'slideshow',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ASGI_APPLICATION = 'hanashi.routing.application'
--- web/slideshow/views.py
+++ web/slideshow/views.py
@@ -1,4 +1,11 @@
+import json
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
 
 def home(request):
     return render(request, 'home.html', {})
+
+def slideshow(request, name):
+    return render(request, 'slideshow.html', {
+        'room_name_json': mark_safe(json.dumps(name))
+    })
