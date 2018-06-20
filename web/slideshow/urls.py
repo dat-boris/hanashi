f87f1ae79f9dcd99732f8e1d843a8fbffe9f0484
sketchytechky <boris@techie.im>
2018-06-20 02:43:04 -0700

    Fix up homepage

--- web/slideshow/urls.py
+++ web/slideshow/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    # url(r'^$', views.home, name='home'),
+    url(r'^$', views.slideshow, name='home'),
     url(r'^s/(?P<name>[^/]+)/$', views.slideshow, name='slideshow'),
 ]
--- web/slideshow/views.py
+++ web/slideshow/views.py
@@ -1,11 +1,13 @@
+import time
 import json
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#     return render(request, 'home.html', {})
 
-def slideshow(request, name):
+def slideshow(request, name=''):
     return render(request, 'slideshow.html', {
-        'room_name_json': mark_safe(json.dumps(name))
+        'room_name_json': mark_safe(json.dumps(name)),
+        'epoch': time.time()
     })
